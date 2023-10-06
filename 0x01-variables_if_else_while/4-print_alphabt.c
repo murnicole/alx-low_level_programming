583583404f16545fa812de96e60b7fe194a4ba36
Nicolous Njoroge <stnicolem@gmail.com>
2023-10-07 02:17:52 +0300

    Alphabet omitted e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Print alphabet in lowercase except q and e
+ *
+ * Return: return 0 (success)
+ */
+int main(void)
+{
+	int alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph == 'q' || alph == 'e')
+		{
+			continue;
+		}
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}	
