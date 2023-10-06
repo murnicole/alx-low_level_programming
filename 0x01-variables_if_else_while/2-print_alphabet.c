7c4c377c642b9393df6fd3a4d96b821d9525ccde
Nicolous Njoroge <stnicolem@gmail.com>
2023-10-07 01:54:29 +0300

    Alphabet using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Alphabet print
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpbt[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i=0; i<26; i++)
+	{
+		putchar(alpbt[i]);
+	}
+	putchar('\n');
+	return (0);
+}
